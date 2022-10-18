cb4d594e990b2f7015f7ac434359134b9b120922
NischayDiwan <nischaydiwan1026@gmail.com>
2022-10-18 17:20:39 +0530

    furhter changes

--- A2/connect4/players/ai.py
+++ A2/connect4/players/ai.py
@@ -33,27 +33,30 @@ class AIPlayer:
         self.type = 'ai'
         self.player_string = 'Player {}:ai'.format(player_number)
         self.time = time
-        self._max_depth = 20
+        self._max_depth = 10
+        self._col_dimension = 0
 
-    def __eval_expectimax(self,player_num: int,state: Tuple[np.array, Dict[int, Integer]]):
+    def __eval_expectimax(self,player_num: int,state: Tuple[np.array, Dict[int, Integer]],move: Tuple[int,bool]):
         x = get_pts(player_num,state[0])
         y = get_pts(3-player_num,state[0])
-        e = x-y
+        c,p = move
+        bc = 0.5*abs(c-self._col_dimension//2)
+        e = x-y-bc
         return e
 
-    def __eval(self,player_num: int,state: Tuple[np.array, Dict[int, Integer]]):
+    def __eval(self,player_num: int,state: Tuple[np.array, Dict[int, Integer]],move: Tuple[int,bool]):
         x = get_pts(player_num,state[0])
         y = get_pts(3-player_num,state[0])
         if(player_num == 1):
-            e = x
+            e = x-y
         else:
-            e = x
+            e = x-y
         return e
 
     def __expectimax_search(self,player_num: int,move: Tuple[int,bool],state: Tuple[np.array, Dict[int, Integer]],depth: int):
         new_state = next_state(player_num,move,state)
         if depth >= self._max_depth:
-            return self.__eval_expectimax(player_num,new_state)
+            return self.__eval_expectimax(player_num,new_state,move)
         opp_moves = get_valid_actions(3-player_num,new_state)
         score = 0.0
         for mov in opp_moves:
@@ -73,7 +76,7 @@ class AIPlayer:
     def __minimax_search(self,player_num: int,move: Tuple[int,bool],state: Tuple[np.array, Dict[int, Integer]],depth: int):
         new_state = next_state(player_num,move,state)
         if depth >= self._max_depth:
-            return self.__eval(player_num,new_state)
+            return self.__eval(player_num,new_state,move)
         opp_moves = get_valid_actions(3-player_num,new_state)
         mini_score = np.inf
         for mov in opp_moves:
@@ -86,12 +89,12 @@ class AIPlayer:
                 maxi_score = max(maxi_score,new_mov_score)
             if len(new_movs) ==0:
                 print("no moves for me")
-                maxi_score = self.__eval(player_num,temp_state) 
+                maxi_score = self.__eval(player_num,temp_state,mov) 
             mini_score = min(mini_score,maxi_score)
             # print("max score i get if opp plays",mov,"at depth :",depth,maxi_score)
         if len(opp_moves)==0:
             print("no moves for opponent")
-            mini_score = self.__eval(player_num,new_state)
+            mini_score = self.__eval(player_num,new_state,move)
         return mini_score
 
     def get_intelligent_move(self, state: Tuple[np.array, Dict[int, Integer]]) -> Tuple[int, bool]:
@@ -113,14 +116,14 @@ class AIPlayer:
         board,popouts=state
         possible_moves = get_valid_actions(self.player_number,state)
         m = max(board.shape[1],len(possible_moves))
-        self._max_depth = math.floor(math.log(self.time,m) + 0.25 * math.log(2*self.time,m))
+        self._max_depth = math.floor(math.log(self.time,m) + 0.5 * math.log(2*self.time,m))
         print(self._max_depth)   
         max_score = -np.inf
         minimax_move = possible_moves[0]
         for move in possible_moves:
             toc = time.perf_counter()
-            if(toc-tic > 9 * (self.time)/10):
-                break
+            # if(toc-tic > 9 * (self.time)/10):
+            #     break
             temp_score = self.__minimax_search(self.player_number,move,state,0)
             # print("thought move at depth :",0,move, temp_score)
             if temp_score > max_score:
@@ -151,6 +154,7 @@ class AIPlayer:
         tic = time.perf_counter()
         board,popouts=state
         possible_moves = get_valid_actions(self.player_number,state)
+        self._col_dimension = board.shape[1]
         m = max(board.shape[1],len(possible_moves))
         self._max_depth = math.floor(math.log(self.time,m) + 0.1 * math.log(2*self.time,m))
         print(self._max_depth)   
